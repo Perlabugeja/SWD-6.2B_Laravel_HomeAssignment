35e1932c9f5f9e7540cb2fc03cc8e43450ac8e82
Perlabugeja <perla_23@icloud.com>
2026-01-09 13:40:53 +0100

    fixed create.blade and show.blade to use the bootstrap template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,39 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Songify')</title>
 
-    <title>Songify</title>
+    <!-- Fonts & Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round">
-    <!-- Bootstrap -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Your app CSS (if using Vite/SCSS) -->
     @vite('resources/sass/app.scss')
-    @vite('resources/js/app.js')
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Songify</a>   
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          @auth
-          <li class="nav-item">
-            <a class="nav-link" href="/playlists">Playlists</a>
-          </li>
-          <li class="nav-item">
-            <form method="POST" action="/logout" class="d-inline">
-              @csrf
-              <button class="btn btn-link nav-link" type="submit">Logout</button>
-            </form>
-          </li>
-          @endauth
-        </ul>
-      </div>
-    </div>
-  </nav>
+</head>
+<body>
 
-    @yield('content')
-  </body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">Songify</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('playlists.index') }}">Playlist</a>
+                </li>
+            </ul>
+            <form method="POST" action="{{ route('logout') }}" class="d-flex">
+                @csrf
+                <button class="btn btn-outline-danger" type="submit">Logout</button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    @vite('resources/js/app.js')
+</body>
 </html>
--- resources/views/playlists/show.blade.php
+++ resources/views/playlists/show.blade.php
@@ -1,16 +1,55 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container mt-5">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<style>
+    body {
+        color: #566787;
+        background: #f5f5f5;
+        font-family: 'Varela Round', sans-serif;
+        font-size: 13px;
+    }
+    .table-wrapper {
+        background: #fff;
+        padding: 20px 25px;
+        margin: 30px 0;
+        border-radius: 3px;
+        box-shadow: 0 1px 1px rgba(0,0,0,.05);
+    }
+    .table-title {        
+        padding-bottom: 15px;
+        background: #435d7d;
+        color: #fff;
+        padding: 16px 30px;
+        margin: -20px -25px 10px;
+        border-radius: 3px 3px 0 0;
+    }
+    .table-title h2 { font-size: 24px; margin: 5px 0 0; }
+    .table-title .btn { color: #fff; font-size: 13px; border: none; min-width: 50px; border-radius: 2px; margin-left: 10px; }
+    table.table tr th, table.table tr td { border-color: #e9e9e9; padding: 12px 15px; vertical-align: middle; }
+    table.table-striped tbody tr:nth-of-type(odd) { background-color: #fcfcfc; }
+    table.table-striped.table-hover tbody tr:hover { background: #f5f5f5; }
+    table.table td a.edit { color: #FFC107; }
+    table.table td a.delete { color: #F44336; }
+</style>
+
+<div class="container">
     <div class="table-wrapper">
-        <div class="table-title mb-3">
+        <div class="table-title">
             <div class="row">
                 <div class="col-sm-6">
                     <h2>Manage <b>Songs</b></h2>
                 </div>
                 <div class="col-sm-6 text-end">
-                    <a href="/songs/create" class="btn btn-success">
-                        Add New Song
+                    <a href="{{ route('songs.create') }}" class="btn btn-success">
+                        <i class="material-icons">&#xE147;</i> Add New Song
                     </a>
                 </div>
             </div>
@@ -24,22 +63,19 @@
                     <th>Actions</th>
                 </tr>
             </thead>
-
             <tbody>
                 @forelse ($songs as $song)
                     <tr>
                         <td>{{ $song->songname }}</td>
                         <td>{{ $song->genre }}</td>
                         <td>
-                            <button class="btn btn-info btn-sm">Edit</button>
-                            <button class="btn btn-danger btn-sm">Delete</button>
+                            <a href="#" class="edit"><i class="material-icons">&#xE254;</i></a>
+                            <a href="#" class="delete"><i class="material-icons">&#xE872;</i></a>
                         </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="3" class="text-center text-muted">
-                            Your playlist is empty. Add your first song 
-                        </td>
+                        <td colspan="3" class="text-center text-muted">Your playlist is empty. Add your first song 🎵</td>
                     </tr>
                 @endforelse
             </tbody>
