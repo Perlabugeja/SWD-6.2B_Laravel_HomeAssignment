a70728aa532d53234f6a81b6b3c5fa7f228f3465
Perlabugeja <perla_23@icloud.com>
2026-01-31 15:07:37 +0100

    removing required from blades

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -12,25 +12,25 @@
 
             <div class="mb-3">
                 <label for="songname" class="form-label">Song Name</label>
-                <input type="text" id="songname" name="songname" class="form-control" value="{{ old('songname') }}" required>
+                <input type="text" id="songname" name="songname" class="form-control" value="{{ old('songname') }}">
                 @error('songname')<div class="text-danger mt-1">{{ $message }}</div>@enderror
             </div>
 
             <div class="mb-3">
                 <label for="artist" class="form-label">Artist/Band</label>
-                <input type="text" id="artist" name="artist" class="form-control" value="{{ old('artist') }}" required>
+                <input type="text" id="artist" name="artist" class="form-control" value="{{ old('artist') }}" >
                 @error('artist')<div class="text-danger mt-1">{{ $message }}</div>@enderror
             </div>
 
             <div class="mb-3">
                 <label for="genre" class="form-label">Genre</label>
-                <input type="text" id="genre" name="genre" class="form-control" value="{{ old('genre') }}" required>
+                <input type="text" id="genre" name="genre" class="form-control" value="{{ old('genre') }}" >
                 @error('genre')<div class="text-danger mt-1">{{ $message }}</div>@enderror
             </div>
 
             <div class="mb-3">
                 <label for="duration" class="form-label">Duration (mm:ss)</label>
-                <input type="text" id="duration" name="duration" class="form-control" value="{{ old('duration') }}" placeholder="03:45" required>
+                <input type="text" id="duration" name="duration" class="form-control" value="{{ old('duration') }}" placeholder="03:45" >
                 @error('duration')<div class="text-danger mt-1">{{ $message }}</div>@enderror
             </div>
 
