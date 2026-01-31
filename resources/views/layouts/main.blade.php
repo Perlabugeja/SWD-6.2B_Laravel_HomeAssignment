2837f68cfeae5ac7ddcb89f34cd3303cdff4a5c1
Perlabugeja <perla_23@icloud.com>
2026-01-31 15:06:23 +0100

    removing songify (hate the name)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,7 +24,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div class="container">
             <a class="navbar-brand fw-bold" href="{{ route('playlists.index') }}">
-                Songify
+            
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
